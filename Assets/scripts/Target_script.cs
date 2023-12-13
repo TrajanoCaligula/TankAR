a9ad1c0ba9f3fa4ed6c4ad7b23e0bbda3ee3b03a
unknown <jaume.malgosa32@gmail.com>
2023-12-13 17:01:40 +0100

    Debugging

--- Assets/scripts/Target_script.cs
+++ Assets/scripts/Target_script.cs
@@ -9,25 +9,25 @@ public class Target_script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        my_object.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (my_object.name == "Vehicle_v3" && !my_object.activeSelf)
+        {
+            my_object.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+        else if (my_object.name == "Vehicle_v1" && !my_object.activeSelf)
+        {
+            my_object.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
     }
 
     void OnBecameVisible()
     {
         my_object.SetActive(true);
-        if(my_object.name == "Vehicle_v3")
-        {
-            my_object.transform.position = new Vector3(0.0f,0.0f,0.0f);
-        }else if(my_object.name == "Vehicle_v1")
-        {
-            my_object.transform.localScale = new Vector3(1.0f,1.0f,1.0f);
-        }
+        
     }
 
     void OnBecameInvisible()
